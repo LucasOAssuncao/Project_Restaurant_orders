030807c722d335d5734163f9de9f39a54dd6031f
Lucas Otoni <lotoni.vivo@gmail.com>
2023-03-13 14:48:18 -0300

    feature: track_orders implementada

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -6,7 +6,7 @@ def analyze_log(path_to_file):
 
     if not path_to_file.endswith('.csv'):
         raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
-    
+
     if not os.path.isfile(path_to_file):
         raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
 
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,46 @@
 class TrackOrders:
-    # aqui deve expor a quantidade de estoque
+    def __init__(self):
+        self.orders = []
+    
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.orders.append({
+            "customer": customer,
+            "order": order,
+            "day": day
+        })
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        dishes = [
+            order["order"] for order in self.orders 
+            if order["customer"] == customer
+        ]
+        return max(set(dishes), key=dishes.count)
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        all_dishes = set(order["order"] for order in self.orders)
+        ordered_dishes = set(
+            order["order"] for order in self.orders 
+            if order["customer"] == customer
+        )
+        return all_dishes - ordered_dishes
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        all_days = set(order["day"] for order in self.orders)
+        visited_days = set(
+            order["day"] for order in self.orders 
+            if order["customer"] == customer
+        )
+        return all_days - visited_days
 
     def get_busiest_day(self):
-        pass
+        days = [order["day"] for order in self.orders]
+        busiest_day = max(set(days), key=days.count)
+        return busiest_day
 
     def get_least_busy_day(self):
-        pass
+        days = [order["day"] for order in self.orders]
+        least_busy_day = min(set(days), key=days.count)
+        return least_busy_day

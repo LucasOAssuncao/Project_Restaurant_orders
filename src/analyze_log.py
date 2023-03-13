bd3fe655cba349ce20d8f0e8f711c56d30330eb9
Lucas Otoni <lotoni.vivo@gmail.com>
2023-03-13 14:18:11 -0300

    analyze_log implementada

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,3 +1,33 @@
+import csv
+import os
+from collections import Counter
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
-    # oi
+
+    if not path_to_file.endswith('.csv'):
+        raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+    
+    if not os.path.isfile(path_to_file):
+        raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
+
+    with open(path_to_file) as file:
+        orders = list(csv.reader(file))
+        items = {order[1] for order in orders}
+        days = {order[2] for order in orders}
+
+        arnaldo_hamburger_orders = sum(1 for order in orders if order[0] == 'arnaldo' and order[1] == 'hamburguer')
+
+        joao_ordered_items = {order[1] for order in filter(lambda x: x[0] == 'joao', orders)}
+        joao_never_ordered_items = items - joao_ordered_items
+        joao_ordered_days = {order[2] for order in filter(lambda x: x[0] == 'joao', orders)}
+        days_joao_never_ordered = days - joao_ordered_days
+
+        maria_items = list(filter(lambda x: x[0] == 'maria', orders))
+        maria_most_ordered_item = max(set(item[1] for item in maria_items), key=[item[1] for item in maria_items].count)
+
+    with open('data/mkt_campaign.txt', "w") as file:
+        print(maria_most_ordered_item, file=file)
+        print(arnaldo_hamburger_orders, file=file)
+        print(joao_never_ordered_items, file=file)
+        print(days_joao_never_ordered, file=file)
